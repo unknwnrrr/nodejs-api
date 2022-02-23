6bbe668691caf4cf2ae7f0c4152e2daa8869e20b
Erick Espinoza <eespinozaa@nextcode.ec>
2022-02-23 15:49:31 -0500

    updating here

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 
 export const testingDatabase = (req: Request, res: Response) => {
   try {
-    const data: string = 'this is my message';
+    const data: string = 'this is my message 2 here';
     // const queryResult = await client.query('SELECT * from alembic_version');
     // const data = queryResult.rows;
     res.send(data);
